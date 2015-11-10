bc1e23a2b766fc6f3276bc8320dd1df6c911ebcf
Peter Pod <peterpod@hotmail.com>
2015-11-10 11:36:28 -0500

    fixed map. It should now always display

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,27 +2,25 @@
 angular.module('app.controllers', [])
 .controller('MapCtrl', function($scope, $ionicLoading) {
  
-    google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(40.4428285, -79.9561175);
- 
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
- 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
-            });
+    var myLatlng = new google.maps.LatLng(40.4428285, -79.9561175);
+
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        var myLocation = new google.maps.Marker({
+            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            map: map,
+            title: "My Location"
         });
- 
-        $scope.map = map;
     });
+
+    $scope.map = map;
  
 });
