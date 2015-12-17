ae5204b08a8eddcc8a32bfb48320cbc3bb87efec
Peter Pod <peterpod@hotmail.com>
2015-12-17 01:14:03 -0500

    Parse cloud code should only push if the alert did not exist before.

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,31 @@
 /* Send a push notification to all users when an alert is added to the database */
 Parse.Cloud.afterSave("Alerts", function(request) {
-  // Our "Alerts" class has a "title" key and description
-  var alertTitle = request.object.get('title');
-  var description = request.object.get('description');
+  // only send push on create not update
+  var createdAt = request.object.get("createdAt");
+  var updatedAt = request.object.get("updatedAt");
+  var objectExisted = (createdAt.getTime() != updatedAt.getTime());
 
-  var pushQuery = new Parse.Query(Parse.Installation);
+  if(!objectExisted){
+    // Our "Alerts" class has a "title" key and description
+    var alertTitle = request.object.get('title');
+    var description = request.object.get('description');
 
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query
-    data: {
-      alert: "New Alert in your area: " + alertTitle,
-      Description: "Description: " + description
-    }
-  	}, {
-    success: function() {
-      // Push was successful
-      console.log("Push was successful");
-    },
-    error: function(error) {
-      throw "Got an error " + error.code + " : " + error.message;
-    }
-  });
+    var pushQuery = new Parse.Query(Parse.Installation);
+
+    Parse.Push.send({
+      where: pushQuery, // Set our Installation query
+      data: {
+        alert: "New Alert in your area: " + alertTitle,
+        Description: "Description: " + description
+      }
+      }, {
+      success: function() {
+        // Push was successful
+        console.log("Push was successful");
+      },
+      error: function(error) {
+        throw "Got an error " + error.code + " : " + error.message;
+      }
+    });
+  }
 });
